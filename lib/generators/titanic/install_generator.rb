c39bbd0954314fb925d4c087cacd1f8c93f096e9
Spencer Dixon <spencerlloyddixon@gmail.com>
2017-05-02 16:06:39 +0100

    Changed name in the stuff being placed in rakefile

--- lib/generators/titanic/install_generator.rb
+++ lib/generators/titanic/install_generator.rb
@@ -3,7 +3,7 @@ module Titanic
   class InstallGenerator < Rails::Generators::Base
     def install
       # Load rake tasks into project
-      content = 'spec = Gem::Specification.find_by_name "titanic"' + "\n" + 'load "#{spec.gem_dir}/lib/tasks/titanic.rake"'
+      content = 'spec = Gem::Specification.find_by_name "titanic-db"' + "\n" + 'load "#{spec.gem_dir}/lib/tasks/titanic.rake"'
       append_to_file 'Rakefile', content
     end
   end
--- lib/titanic/version.rb
+++ lib/titanic/version.rb
@@ -1,3 +1,3 @@
 module Titanic
-  VERSION = "0.0.4"
+  VERSION = "0.0.5"
 end
